812b09407faecf29e35f6d61d0f8011f8b923dd5
themdshakibul <mdshakibul88240@gmail.com>
2026-01-26 09:43:31 +0600

    problem solve ture false bolean Data type

---
+++ true-false.js
@@ -0,0 +1,61 @@
+// Javascript Bolean Data Type Problem Practice
+
+// 1. ধর তোর বন্ধু পরীক্ষায় বাম্বু খাইছে। এখন সে হ্যাপি কি না, সেটা একটা boolean টাইপের ভেরিয়েবল ডিক্লেয়ার করে সেটার মান রাখ। আর সেটা console-এ প্রিন্ট কর।
+// Ans:
+const Happy = true;
+console.log("My Friend is ", Happy);
+
+// 2. তুই নিজে একটা প্রোগ্রাম লিখে দেখ, তোর crush তোকে মেসেজ দিয়েছে কি না। ধর মেসেজ দিলে true, না দিলে false। সেটা প্রিন্ট কর console.log() দিয়ে।
+// Ans:
+const crush = true;
+console.log("Crush Message is :", crush);
+
+// 3. তোর প্রিয় টিম খেলায় জিতে গেল, এখন সেই খুশিতে বগল বাজাতে বাজাতে celebrating নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার করে সেটার মান দে, আর আউটপুট দেখ।
+
+// Ans:
+const celebrating = true;
+console.log("Means you are celebrating :", celebrating);
+
+// 4. তোর এখন boolean টাইপ সম্পর্কে ভালো ধারণা হয়েছে। এবার একটা প্রোগ্রাম লিখ, যেখানে তুই তিনটা boolean ভেরিয়েবল ডিক্লেয়ার করবি: "isHappy", "hasMoney", আর "isHungry"। এই তিনটার মান যথাক্রমে true, false, আর true সেট করবি। তারপর আউটপুট দে console.log() দিয়ে।
+
+// Ans:
+const isHappy = true;
+const hasMoney = false;
+const isHungry = true;
+
+console.log("Iam Happy :", isHappy);
+console.log("Has Money :", hasMoney);
+console.log("Is Hungry :", isHappy);
+
+// 5. তোর আজকে পড়া শেষ হয়েছে কি না? পড়া শেষ হলে true, না হলে false হবে। আর সেটা রাখার জন্য isStudyDone নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর।
+
+// Ans:
+const isStudyDone = false;
+
+if (isStudyDone) {
+  console.log("Study finished :", true);
+} else {
+  console.log("Study not finished :", false);
+}
+
+// 6. এখন isPhoneCharged নামে একটা boolean ভেরিয়েবল ডিক্লেয়ার কর। চার্জ শেষ হলে true, শেষ না হলে false।
+
+// Ans:
+const isPhoneCharged = true;
+
+if (isPhoneCharged) {
+  console.log("Phone is Charged :", true);
+} else {
+  console.log("Not Phone Charged :", false);
+}
+
+// 7. তোর কি রাতের খাওয়া শেষ? সেটা নিয়ে isDinnerDone নামে একটি boolean ভেরিয়েবল ডিক্লেয়ার কর। খাওয়া শেষ হলে কী লিখবি, আর শেষ না হলে কী লিখবি, সেটা তো জানোস অলরেডি।
+
+// Ans:
+const isDinnerDone = true;
+
+if (isDinnerDone) {
+  console.log("Dinner is finished :", true);
+} else {
+  console.log("Dinner not finished :", false);
+}
