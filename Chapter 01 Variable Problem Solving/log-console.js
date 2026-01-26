dc954428dd97d244c3b23797099528539be6adfd
themdshakibul <mdshakibul88240@gmail.com>
2026-01-26 08:39:49 +0600

    first commit

---
+++ Chapter 01 Variable Problem Solving/log-console.js
@@ -0,0 +1,46 @@
+// Javascript Variable Problem Practice:
+
+// 1. তোর বর্তমান বয়স age নামে একটি ভেরিয়েবলে রাখ। কনসোলে তারপর এই
+// ভেরিয়েবলটি প্রিন্ট কর।
+// Ans:
+
+const age = 19;
+console.log("My Current age :", age); //19
+
+// 2. তুই এই বছর যে কয়টা বই পড়ার পরিকল্পনা করছস, সেটাকে booksToRead নামে একটি ভেরিয়েবলে রাখ। পরে booksToRead-এর মান কনসোলে আউটপুট দেখা।
+// Ans:
+
+const booksToRead = 18;
+console.log("The number of books Plan to read this year :", booksToRead); //18
+
+// 3. তুই প্রতিদিন সকাল কয়টার সময় ঘুম থেকে উঠস, সেটাকে wakeUpTime নামে একটি ভেরিয়েবলে ডিক্লেয়ার কর। তারপর wakeUpTime-এর নতুন মান সেট কর এবং কনসোলে আউটপুট হিসেবে দেখা।
+// Ans:
+
+const wakeUpTime = 6;
+console.log("The time my wake up every morning :", wakeUpTime);
+
+// 4. আজ সারাদিনে কতঘণ্টা সময় তুই মোবাইল ফোনে নষ্ট করছস, সেটাকে wastedTime নামে একটি ভেরিয়েবলে এই মান রাখ। তারপর এইটাকে কনসোলে দেখা।
+
+// Ans:
+
+const wastedTime = 5;
+console.log("Today wastedTime is my phone :", wastedTime);
+
+// 5. এই মুহূর্তে তুই কত টাকা ধার করে রাখছস? সেটাকে borrow নামে একটি ভেরিয়েবলে রাখ।
+
+// Ans:
+
+const borrow = 0;
+console.log("Borrowed money amount :", borrow);
+
+// 6. তোর প্রিয় টিম অপোজিট টিমকে সর্বোচ্চ কয়টা গোল দিছে এক ম্যাচে। সেটা দিয়ে goals নামে একটা ভেরিয়েবলে লিখে সেটাকে আউটপুট হিসেবে দেখা।
+
+// Ans:
+const goals = 5;
+console.log("Height goals score in one match :", goals);
+
+// 7.তোর বিছানায় কয়টা বালিশ আছে, সেটার জন্য pillow নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর কনসোলে সেটাকে আউটপুট হিসেবে দেখা।
+
+// Ans:
+const pillow = 2;
+console.log("Number of the pollow on the bed :", pillow);
---
+++ Chapter 01 Variable Problem Solving/string.js
@@ -0,0 +1,39 @@
+// JS String Problem  Practice
+
+// 1. এখন name নামে একটা ভেরিয়েবল ডিক্লেয়ার কর, আর যেটার মান হবে তোর নাম। এরপর সেটাকে console-এ আউটপুট হিসেবে দেখা।
+// Ams:
+const name = "Md Shakibul Islam";
+console.log("My name is :", name);
+
+// 2. তোর ঠিকানা (address) একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার করে দেখ। আর সেটাকে console.log() দিয়ে আউটপুট দেখ।
+// Ans:
+const address = "Manikgonj Dhaka Bangladesh";
+console.log("My address :", address);
+
+// 3. তোর বেস্ট ফ্রেন্ড কোন স্কুল, কলেজ বা ভার্সিটিতে পড়ে, সেই স্কুল, কলেজ বা ভার্সিটির নামে একটা ভেরিয়েবল ডিক্লেয়ার কর। তারপর সেটাকে আউটপুট হিসেবে দেখা।
+// Ans:
+const bestFriendUniversity = "Deffodil International university";
+console.log("My best Friend university name is :", bestFriendUniversity);
+
+// 4. তোর প্রিয় নায়িকার নাম fan নামে একটা ভেরিয়েবলে রাখ। তারপর কনসোলে দেখা।
+// Ans:
+const fan = "Tanjin Trisha";
+console.log("My Faverit fan name is :", fan);
+
+// 5. এই বছরের তোর প্রিয় সিনেমার নাম movieOfTheYear নামে একটা ভেরিয়েবলে রাখ। তারপর সেটা কনসোলে প্রিন্ট কর।
+
+// Ans:
+const movieOfTheYear = "Money Heist";
+console.log("Faverit movie is :", movieOfTheYear);
+
+// 6. সবার কিছু শখ বা hobby থাকে। তোর শখ বা hobby নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর।
+// Ans:
+
+const hobby = "Programer";
+console.log("My hobby is a ", hobby);
+
+// 7. তোর প্রিয় রং কোনটা? color নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর।
+// Ans:
+
+const color = "Pink";
+console.log("My Faverit colore is :", color);
