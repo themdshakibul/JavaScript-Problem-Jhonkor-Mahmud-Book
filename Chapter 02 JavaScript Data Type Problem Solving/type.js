b2ca6aaf34dc8495f0306277c4b7dffaea1b9eec
themdshakibul <mdshakibul88240@gmail.com>
2026-01-26 13:12:25 +0600

    chapter 02 problem solving

---
+++ Chapter 02 JavaScript Data Type Problem Solving/type.js
@@ -0,0 +1,45 @@
+/**
+ * 
+ * ভেরিয়েবল বিভিন্ন ধরনের হতে পারে। যেমন:
+  সংখ্যা (Number): 1, 2, 3, 4, 5 ইত্যাদি।
+  স্ট্রিং (String): "বাংলা", "English", "Hello World" ইত্যাদি।
+  বুলিয়ান (Boolean): true, false।
+ * 
+ * 
+ */
+
+// Javascript Data Type Problem Practice
+
+// 1."typeof" ব্যবহার করে প্রোগ্রাম লিখে দেখ, কোন ভেরিয়েবলের মান কী ধরনের। ধর, একটা ভেরিয়েবল "price" ডিক্লেয়ার কর, যার মান 555, এখন console-এ এই টাইপ চেক কর।
+// Ans:
+const price = 555;
+console.log(typeof price);
+
+// 2. একটা ভেরিয়েবল "learning" ডিক্লেয়ার কর, যার মান true, আর সেটা console.log() দিয়ে আউটপুট কর। পরে "typeof" ব্যবহার করে দেখ, এটা কী ধরনের ভেরিয়েবল।
+// Ans:
+const larning = true;
+console.log(typeof larning);
+
+// 3. megaCity নামক একটা ভেরিয়েবলের মধ্যে মান হিসেবে "Dhaka" আছে। এইবার "typeof" ব্যবহার করে দেখ, এই ভেরিয়েবলটা কী ধরনের।
+
+// Ans:
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+// 4. খুব কাকফাটা গরমে কপালে হাত দিয়ে দেখলি, গা পুড়ে যায় যায় অবস্থা। সেই মুহূর্তেই তুই "temperature" নামে একটা ভেরিয়েবল ডিক্লেয়ার করলি, যার মান 102.5। এটা কী ধরনের ভেরিয়েবল, সেটা "typeof" দিয়ে চেক কর।
+
+// Ans:
+const temperature = 102.5;
+console.log(typeof temperature);
+
+// 5. ক্ষিধার চোটে তুই "isHungry" নামে একটা ভেরিয়েবল বানায় ফেললি, আর সেটার মান true। এখন console.log() আর "typeof" ব্যবহার করে এর টাইপ দেখ।
+
+// Ans:
+const isHungry = true;
+console.log(typeof isHungry);
+
+// 6. একটা ভেরিয়েবলের নাম "isRich", যার মান true। এখন console-এ এইটার টাইপ চেক কর।
+
+// Ans:
+const isRich = true;
+console.log(typeof isRich);
