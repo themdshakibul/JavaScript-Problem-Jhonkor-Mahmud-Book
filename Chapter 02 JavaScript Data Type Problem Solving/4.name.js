f0d6315cf9bf909a614acb72f7a6f32d892831ed
themdshakibul <mdshakibul88240@gmail.com>
2026-01-30 16:29:57 +0600

    JavaScript maming Practice

---
+++ Chapter 02 JavaScript Data Type Problem Solving/4.name.js
@@ -0,0 +1,59 @@
+// JavaScript Reserved  (keyword)
+// Javascript naming Practice:
+
+// 1. তুই একটা প্রোগ্রাম লিখে দেখ, যেখানে তুই ভেরিয়েবল ডিক্লেয়ার করবি camelCase ব্যবহার করে। ধর, তুই তোর প্রিয় খাবার নামে একটা স্ট্রিং ভেরিয়েবল ডিক্লেয়ার কর। আর সেটার মান দে।
+
+//! Ans:
+const myFaveriteFood = "Biriyani";
+
+// 2. তুই দেখ, ভেরিয়েবলের নাম case-sensitive কি না। একটা প্রোগ্রাম লিখে দুইটা ভেরিয়েবল ডিক্লেয়ার করবি, একটার নাম "city", আরেকটার নাম "City"। প্রথমটার মান "Chittagong" আর দ্বিতীয়টার মান "Dhaka" দে। console.log() দিয়ে দুইটা আউটপুট দেখ।
+
+//! Ans:
+let city = "Chittagong";
+let City = "Dhaka";
+
+console.log(city);
+console.log(City);
+
+// 3. তুই userName নামে একটা ভেরিয়েবল ডিক্লেয়ার করবি এবং তার মান Johan দে। এবার username নামে আরেকটা ভেরিয়েবল ডিক্লেয়ার করবি এবং তার মান Jovan দে। দুইটা আউটপুট দেখ console.log() দিয়ে।
+
+//! Ans:
+let userName = "Johan";
+let username = "Jovan";
+
+console.log(userName);
+console.log(username);
+
+// 4. আমার বাসার ঠিকানা (বাসরের ঠিকানা বলি নাই কিন্তু) নামে ইংরেজিতে একটা ভেরিয়েবল ডিক্লেয়ার করবি। camelCase ব্যবহার করে ভেরিয়েবল ডিক্লেয়ার করবি। তারপর এইটার স্ট্রিং একটা মান সেট করবি।
+
+//! Ans:
+
+let myHomeAddress = "Manikgonj Dhaka Bangladesh";
+console.log(myHomeAddress);
+
+// 5. সুপার ওভারে কত রান হইছে, সেটার জন্য super নামে একটা ভেরিয়েবল ডিক্লেয়ার কর এবং মান সেট কর। করার পর যে আউটপুট দেখতে পাচ্ছস, সেটা কি ঠিক আছে নাকি কোনো সমস্যা আছে।
+
+//! Ans:
+//? super JavaScript ar reserved keyword
+// let super = 10;
+// console.log(super)
+
+// 6. তুই কোন ক্লাসে পড়স, সেটা লেখার জন্য class নামে একটা ভেরিয়েবল লিখ। তারপর আউটপুট এ দেখ, কী দেখায়।
+
+//! Ans:
+
+//? class JavaScript ar reserved keyword
+// let class = 12
+// console.log(class)
+
+// 7. সেকেন্ড পজিশনে কে ব্যাট করবে, সেটার জন্য 2ndPosition নামে একটা ভেরিয়েবল লিখে সেখানে একজন ব্যাটসম্যানের নাম লিখে দে। দেখ আউটপুট দেখায় কি না। যদি সমস্যা করে, তাহলে কী করা লাগে, সেটা কমেন্ট করে ভেরিয়েবলের ওপরে লিখ।
+
+//! Ans:
+
+//! variable name kokhon number diye suru hoy na
+//? Solution
+//! i. secondPosition = "Rokibul"
+//! ii. Position2nd = "Rokibul"
+
+// let 2ndPosition = "Rokibul"
+// console.log(2ndPositionj)
